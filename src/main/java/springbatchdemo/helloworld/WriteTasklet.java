40d54a7385eb6e16d3693b91a927f2660cb48e3f
qiaor <larry_qr@126.com>
2015-03-13 14:36:11 +0800

    simple demo
    refer
    link:http://www.cnblogs.com/gulvzhe/archive/2011/10/31/2230655.html
    Update something as the spring batch version upgrading.

---
+++ src/main/java/springbatchdemo/helloworld/WriteTasklet.java
@@ -0,0 +1,23 @@
+package springbatchdemo.helloworld;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class WriteTasklet implements Tasklet {
+
+    private String message;
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+	System.out.println("WriteTasklet: " + message);
+	return RepeatStatus.FINISHED;
+    }
+    
+
+}
